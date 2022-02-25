661ff14ef354d42ce930821de063c87c28a4a6c1
Spacantho <anthony.tourolle08@gmail.com>
2022-02-25 13:33:48 +0100

    commit

---
+++ assets/includes/navbar.php
@@ -0,0 +1,44 @@
+    <nav>
+
+        <div class="nav-menug">
+            <div class="nav-logo"><img src="assets/img/navbar/logo.png" alt="logo"></div>
+
+            <div class="nav-menu">
+                
+                   <a href="index.php">Accueil</a>
+                    <a href="classement.php">Classement</a>
+                    <a href="inscription.php">Inscription</a>
+                    <a id="nav-connexion" class="nav-connexion">Connexion</a>
+             
+                    <div id="connexionsite" class="connexionsite">
+<form>
+<label> Login : </label><br>
+<input type="text" placeholder="Votre login"><br><br>
+<label> Password : </label><br>
+<input type="password" placeholder="Votre Password"><br><br>
+<input type="submit" value="Connexion">
+
+</form>
+                    
+
+                    </div>
+
+            </div>
+
+            <div id="nav-burger" class="nav-burger">☰
+
+            <div id="nav-burger-list" class="nav-burger-list">
+                <div class="nav-burger-list2">
+                    <a href="index.php">Accueil</a>
+                    <a href="classement.php">Classement</a>
+                    <a href="inscription.php">Inscription</a>
+                    <a href="connexion.php">Connexion</a>
+            </div>
+            </div>
+
+
+            </div>
+        </div>
+
+    </nav>
+    <script src="assets/js/includes.js"></script>
